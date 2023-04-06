de22ed852f27d4f46d1eea4638d73e054e8a86ae
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2023-04-05 20:46:52 -0400

    added to dictionary and know wont display non dictionary events

--- dist/signpost.js
+++ dist/signpost.js
@@ -4,80 +4,94 @@ const calendarUrl = "https://standrewspolaris.org/events/month/?ical=1";
 const currentDate = new Date(2018, 11, 1, 17, 0, 0);
 
 const locationDict = {
-  "Sanctuary": "arrow-straight",
-  "Room 101": "arrow-left",
-  "Gathering Area": "arrow-left",
+	"Sanctuary": "arrow-straight",
+	"Room 101": "arrow-left",
+	"Room 102": "arrow-left",
+	"Gathering Area": "arrow-left",
+
+	"Room 105": "arrow-right",
+	"Room 107": "arrow-right",
+
+	"Parish House": "parish-house",
 };
 
 fetch(`/.netlify/functions/node-fetch?url=${encodeURIComponent(calendarUrl)}`)
-  .then((response) => response.text())
-  .then((data) => {
-    const jCalData = ICAL.parse(data);
-    const comp = new ICAL.Component(jCalData);
-    const vevents = comp.getAllSubcomponents("vevent");
+	.then((response) => response.text())
+	.then((data) => {
+		const jCalData = ICAL.parse(data);
+		const comp = new ICAL.Component(jCalData);
+		const vevents = comp.getAllSubcomponents("vevent");
 
-    const events = vevents
-      .map((vevent) => {
-        const event = new ICAL.Event(vevent);
-        // console.log(event.location);
-        const start = event.startDate.toJSDate();
-        const end = event.endDate.toJSDate();
+		const events = vevents
+			.map((vevent) => {
+				const event = new ICAL.Event(vevent);
+				// console.log(event.location);
+				const start = event.startDate.toJSDate();
+				const end = event.endDate.toJSDate();
 
-        if (
-			true // TODO: Remove this line to enable date filtering
-        //   start.getDate() === currentDate.getDate() &&
-        //   start.getMonth() === currentDate.getMonth() &&
-        //   start.getFullYear() === currentDate.getFullYear() &&
-        //   end.getDate() === currentDate.getDate() &&
-        //   end.getMonth() === currentDate.getMonth() &&
-        //   end.getFullYear() === currentDate.getFullYear()
-        ) {
-          return {
-            summary: event.summary,
-            uid: event.uid,
-            description: event.description,
-            start: start,
-            end: end,
-            location: event.location,
-          };
-        } else {
-          return null;
-        }
-      })
-      .filter((event) => event !== null);
+				if (
+					true // TODO: Remove this line to enable date filtering
+					//   start.getDate() === currentDate.getDate() &&
+					//   start.getMonth() === currentDate.getMonth() &&
+					//   start.getFullYear() === currentDate.getFullYear() &&
+					//   end.getDate() === currentDate.getDate() &&
+					//   end.getMonth() === currentDate.getMonth() &&
+					//   end.getFullYear() === currentDate.getFullYear()
+				) {
+					return {
+						summary: event.summary,
+						uid: event.uid,
+						description: event.description,
+						start: start,
+						end: end,
+						location: event.location,
+					};
+				} else {
+					return null;
+				}
+			})
+			.filter((event) => event !== null);
 
-    const eventTable = document.getElementById("event-table");
-    const tbody = eventTable.querySelector("tbody");
+		const eventTable = document.getElementById("event-table");
+		const tbody = eventTable.querySelector("tbody");
 
-	events.forEach(event => {
-		const row = tbody.insertRow();
-	
-		const summaryCell = row.insertCell();
-		summaryCell.textContent = event.summary;
-	
-		const timeCell = row.insertCell();
-		const timeFormat = new Intl.DateTimeFormat('en-US', { timeStyle: 'short' });
-		const startTimeString = timeFormat.format(event.start);
-		const endTimeString = timeFormat.format(event.end);
-		timeCell.textContent = `${startTimeString} - ${endTimeString}`;
-	
-		const locationCell = row.insertCell();
+		events.forEach((event) => {
+			let direction = null;
 
-		let direction = null;
+			Object.keys(locationDict).forEach((key) => {
+				if (event.location.includes(key)) {
+					direction = locationDict[key];
+				}
+				console.log(`Direction for location ${event.location}: ${direction}`);
+			});
 
-		Object.keys(locationDict).forEach(key => {
-			if (event.location.includes(key)) {
-			  direction = locationDict[key];
-			  console.log(`Direction for location ${event.location}: ${direction}`);
+			if (direction !== null) {
+				const row = tbody.insertRow();
+
+				const summaryCell = row.insertCell();
+				summaryCell.textContent = event.summary;
+
+				const timeCell = row.insertCell();
+				const timeFormat = new Intl.DateTimeFormat("en-US", {
+					timeStyle: "short",
+				});
+				const startTimeString = timeFormat.format(event.start);
+				const endTimeString = timeFormat.format(event.end);
+				timeCell.textContent = `${startTimeString} - ${endTimeString}`;
+
+				const locationCell = row.insertCell();
+
+				const arrow = document.createElement("span");
+				arrow.classList.add("arrow");
+				arrow.classList.add(direction);
+				locationCell.appendChild(arrow);
+				locationCell.appendChild(
+				document.createTextNode(
+					Object.keys(locationDict).find(k => locationDict[k] === direction)
+				)
+				);
 			}
-		  });
 			
-		const arrow = document.createElement('span');
-		arrow.classList.add('arrow');
-		arrow.classList.add(direction);
-		locationCell.appendChild(arrow);
-		locationCell.appendChild(document.createTextNode(event.location || 'TBD'));
-	});
-	
-  })
-  .catch((error) => console.error(error));
+		});
+	})
+	.catch((error) => console.error(error));
